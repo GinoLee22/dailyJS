0cbc41e551c9b1c671e26f6039663584088dc809
ginolee22 <ginolee22@outlook.com>
2020-08-16 16:23:59 -0700

    [js][example][2/2]weird behavior of func bind

--- examples_func_bind.js
+++ examples_func_bind.js
@@ -1,3 +1,6 @@
+/**
+ * Random code
+ */
 let dog = {
   sound: 'woof',
   talk: function () {
@@ -11,3 +14,21 @@ talkFunction();   // undefine
 
 let boundTalkFunction = dog.talk.bind(dog);
 boundTalkFunction();    // 'woof'
+
+/**
+ * Weird behavior of bind
+ */
+let talk = function () {
+  console.log(this.sound);
+}
+let boromir = {
+  talk: talk.bind(this),
+  sound: 'haha',
+  test: function() {
+    console.log('what is this:', this);
+  }
+}
+boromir.talk();   // undefined. I don't know what's wrong
+boromir.test();   // 'boromir' object
+
+console.log('function talk', boromir.talk);

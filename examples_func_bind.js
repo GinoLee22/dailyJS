0615ca71eeb8a8dc7c398a581262d857610a662e
ginolee22 <ginolee22@outlook.com>
2020-08-16 21:47:17 -0700

    [js][example][3/3]func bind understanding this

--- examples_func_bind.js
+++ examples_func_bind.js
@@ -16,19 +16,28 @@ let boundTalkFunction = dog.talk.bind(dog);
 boundTalkFunction();    // 'woof'
 
 /**
- * Weird behavior of bind
+ * Weird behavior of bind.
  */
 let talk = function () {
-  console.log(this.sound);
+  console.log('talk', this.sound);
 }
+// This will never work because during bind `this` is  the global this
 let boromir = {
-  talk: talk.bind(this),
-  sound: 'haha',
-  test: function() {
+  test: function () {
     console.log('what is this:', this);
-  }
+  },
+  sound: 'haha',
+  talk: talk.bind(this),
 }
-boromir.talk();   // undefined. I don't know what's wrong
-boromir.test();   // 'boromir' object
 
-console.log('function talk', boromir.talk);
+boromir.test();   // 'boromir' object
+boromir.talk();   // undefined. I don't know what's wrong
+
+// Best usage of function `bind`. During initializing, this is an object already
+class TestClass {
+  talk = talk.bind(this);
+  sound = 'testClass';
+}
+
+let testClass = new TestClass();
+testClass.talk();
